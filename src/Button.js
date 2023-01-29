edb7dc5814d7fbf0fd1b40ca44853560d5dfef23
wooyoung kim <wy1615@naver.com>
2023-01-29 21:39:51 +0900

    useEffct를 사용하여 컴포넌트의 rendering을 제어, state가 변화가 있을때만 실행하도록 설정

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types";
 //Proptypes 을 인스톨 npm install --save prop-types 하고 임폴트 작성
 import styles from "./Button.module.css";
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+function Button({ text, changeValue }) {
+  return (
+    <div>
+      <button onClick={changeValue} className={styles.btn}>
+        {text}
+      </button>
+    </div>
+  );
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
 };
+
 export default Button;
