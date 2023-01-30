d62834acb751399d5f3ecfb6d1feb133ae21e4c9
wooyoung kim <wy1615@naver.com>
2023-01-30 17:56:25 +0900

    useEffect로 제어중인 컴포넌트가 destroyed 될때 unmount시 함수 실행방법

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 /*useEffect*/
+function Hello() {
+  /*function byFn() {
+    console.log("bye");
+  }
+  function hiFn() {
+    console.log("hi");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  useEffect는 function을 받고 이 function은 dependency가 변화할때 호출.
+  위 경우 dependency가 비어있으니, 컴포넌트가 처음 생성될 때 function이
+  호출된 후에 다시 호출하지 않음. component가 파괴될 때도 function을 실행하고
+  싶으면, hiFn이 byFn을 return 해야 함 react.js가 hiFn을 실행하고 
+  component가 파괴될 때는 react.js는 hiFn이 return 한 function을 실행함.
+  -> 왜냐면 deps가 비어있으면 자동으로 컴포넌트가 파괴될 때 cleanup함수가 
+  실행되는데 그 과정이 리렌더링으로 useEffect함수가 실행되고 클린업하면서
+   이전에 있던 이펙트인 console.log(“created :) )가 삭제되고 
+   새로운 이펙트 함수인 return함수가 실행되기 때문이다.
+리렌더링 -> 이전 이펙트 클린업 -> 이펙트 실행
+
+  */
+  useEffect(() => {
+    console.log("i'm here!");
+    return () => console.log("destroyed :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [value, setValue] = useState(0);
-  const [keyword, setKeword] = useState("");
-  const changeValue = () => setValue((prev) => prev + 1);
-  const onChage = (event) => {
-    setKeword(event.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
   };
-  useEffect(() => {
-    console.log("컴포넌트 처음 생성시에만 실행");
-  }, []);
-  useEffect(() => console.log("Call the API..."), []);
-  /*컴포넌트가 생성되는 첫 시점에 
-  useEffect 코드가 딱 한번만 실행될수 있도록 보호해줌*/
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 5) {
-      console.log("Search for ", keyword);
-    }
-    /*keyword가 변화할때만 항상 코드가 실행이 됨
-  키워드가 빈값이 아니고, keyword의 길이가 5 이상일때만 실행하도록 설정
-  */
-  }, [keyword]);
-  useEffect(() => {
-    console.log("카운터가 변할때만 실행");
-  }, [value]);
-  useEffect(() => {
-    console.log("카운터 or 키워드가 변화할 시 실행되도록 코딩");
-  }, [keyword, value]);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChage}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}> {value}</h1>
-      <Button changeValue={changeValue} text={"+"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
-
 export default App;
