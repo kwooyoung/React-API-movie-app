f76983340d6d7dfabf505392ecca887e179b85b4
wooyoung kim <wy1615@naver.com>
2023-01-31 21:04:25 +0900

    나머지 매개변수와 map을 이용하여 투두리스트 표현

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
-
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    //나머지 매개변수를 이용해서 작성한 toDo 리스트를 toDos에 저장
+    setToDo("");
   };
-
+  console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
-          onChange={onChange}
           value={toDo}
+          onChange={onChange}
           type="text"
           placeholder="오늘 해야할 일을 적어주세요"
         />
-        <button>Add</button>
+        <button>추가</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
