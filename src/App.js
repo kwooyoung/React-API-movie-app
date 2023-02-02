0ec9669298879fa0f4e178d820c25082d30a918b
wooyoung kim <wy1615@naver.com>
2023-02-02 15:47:18 +0900

    Coins price를 USD => input으로 원하는 가격(USD)에 맞는 BTC 개수로 알려주는 기능 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [amount, setAmount] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -14,17 +16,50 @@ function App() {
         //setCoins에 json으로 변경한 정보를 넣으면서 로딩 null값으로 변경
       });
   }, []);
+
+  const handleInput = (event) => {
+    setAmount(event.target.value);
+  };
+  console.log("amount", amount);
+
+  const handlePrice = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPrice(coins[value].quotes.USD.price);
+  };
+
   return (
     <div>
-      <h1>The Coins ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price.toFixed(3)} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={handlePrice}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.rank - 1}>
+              {coin.name} ({coin.symbol})
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <div>
+        <h2> 1COIN = {price} USD</h2>
+        <span>
+          <input
+            type="number"
+            placeholder="USD"
+            value={amount}
+            onChange={handleInput}
+          />{" "}
+          USD
+        </span>
+        <span> = </span>
+        <strong>
+          {amount / price} {amount / price <= 1 ? "COIN" : "COINS"}
+        </strong>
+      </div>
     </div>
   );
 }
