39e0996da7bd5364921a9f5618c7f7edf20d856d
wooyoung kim <wy1615@naver.com>
2023-02-02 10:03:43 +0900

    비트코인의 API 정보를 fetch하여 데이터를 추출, 정보 표현 기능 생성

--- src/App.js
+++ src/App.js
@@ -2,33 +2,27 @@ import { useState, useEffect } from "react";
 /*useEffect*/
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDo);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      //패치한 APi정보를 json으로 변경
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        //setCoins에 json으로 변경한 정보를 넣으면서 로딩 null값으로 변경
+      });
+  }, []);
   return (
     <div>
-      <h1>오늘 할일 ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="오늘 해야할 일을 적어주세요"
-        />
-        <button>추가</button>
-      </form>
-      <hr />
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price.toFixed(3)} USD
+          </li>
         ))}
       </ul>
     </div>
