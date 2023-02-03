9988ea14634cf47d3cf000ca1e552fdd07b6781b
wooyoung kim <wy1615@naver.com>
2023-02-03 17:20:14 +0900

    async await 학습과 사용으로 API fetch movie api 활용

--- src/App.js
+++ src/App.js
@@ -3,85 +3,51 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [price, setPrice] = useState(0);
-  const [amount, setAmount] = useState(0);
-  const KW = Math.floor(price * 1222).toLocal;
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  /* 
+  const json = await (await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    )
+    ).json();
+    const json = await response.json();//위처럼 변경하면 이 라인은 지워도 됨
+    setMovies(json.data.movies);
+    setLoading(false);
+  };*/
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      //패치한 APi정보를 json으로 변경
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-        //setCoins에 json으로 변경한 정보를 넣으면서 로딩 null값으로 변경
-      });
+    getMovies();
   }, []);
 
-  const handleInput = (event) => {
-    setAmount(event.target.value);
-  };
-  console.log("amount", amount);
-
-  const handlePrice = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPrice(coins[value].quotes.USD.price);
-  };
-  const kw = Math.floor(price * 1222).toLocaleString();
-
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={handlePrice}>
-          {coins.map((coin) => (
-            <option key={coin.id} value={coin.rank - 1}>
-              {coin.name} ({coin.symbol})
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title_long}</h2>
+
+              <p>{movie.summary}</p>
+
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <hr />
-      <div>
-        <h2>
-          {" "}
-          1COIN = {price} USD {`(약 ${kw}원)`}
-        </h2>
-      </div>
-      <span>
-        <input
-          type="text"
-          placeholder="USD"
-          value={amount}
-          onChange={handleInput}
-        />{" "}
-        USD
-      </span>
-      <span> = </span>
-      <strong>
-        {amount / price} {amount / price <= 1 ? "COIN" : "COINS"}
-      </strong>
-      <div>
-        <hr />
-        <span>
-          <input
-            type="text"
-            placeholder="USD"
-            value={amount}
-            onChange={handleInput}
-          />{" "}
-          원
-        </span>
-        <span> = </span>
-        <strong>
-          {amount / price / 1222} {amount / price <= 1 ? "COIN" : "COINS"}
-        </strong>
-      </div>
     </div>
   );
 }
