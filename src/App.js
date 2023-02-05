fc817444fb513a14e400507d97e718f8ecccf9c3
wooyoung kim <wy1615@naver.com>
2023-02-05 23:41:02 +0900

    react-router-dom 을 사용하여 page control, 스위치는 Route(url)  찾는데, Route는 url을 의미 and Route를 찾으면 컴포넌트를 렌더링

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 /*useEffect*/
-import Movie from "./Movie";
-
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  /*  
-  const json = await (await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-    )
-    ).json();
-    const json = await response.json();//위처럼 변경하면 이 라인은 지워도 됨
-    setMovies(json.data.movies);
-    setLoading(false);
-  };*/
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading... </h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              title_long={movie.title_long}
-              summary={movie.summary}
-              url={movie.url}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
